5aeb992feabe859a8926c1ba52fec58f413e43f2
Ronald Anthonissen <Ronald.Anthonissen@ventigrate.be>
2015-01-05 13:11:17 +0100

    Project creation
    StringExtensions

---
+++ Anthoro.NetExensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Anthoro.NetExensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNOtNullOrWhiteSpace(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
